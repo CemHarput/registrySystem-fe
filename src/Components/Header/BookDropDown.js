c5b66557ef0cf65aed59aaa4055c0b31b5cb8ad2
CemHarput <cemharput@gmail.com>
2023-11-26 17:55:47 +0300

    Modal Component Added

--- src/Components/Header/BookDropDown.js
+++ src/Components/Header/BookDropDown.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
-const BookDropDown = () => {
+const BookDropDown = ({ onAddBookClick }) => {
   return (
     <div
-      className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+      className="absolute left-1/2 transform -translate-x-1/2 top-full z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
       role="menu"
       aria-orientation="vertical"
       aria-labelledby="user-menu-button"
@@ -14,9 +14,11 @@ const BookDropDown = () => {
         className="block px-4 py-2 text-sm text-gray-700"
         role="menuitem"
         tabIndex="-1"
+        onClick={onAddBookClick}
       >
         Add a Book
       </a>
+
       <a
         href="#"
         className="block px-4 py-2 text-sm text-gray-700"
@@ -24,6 +26,14 @@ const BookDropDown = () => {
         tabIndex="-1"
       >
         Show all Orders
+      </a>
+      <a
+        href="#"
+        className="block px-4 py-2 text-sm text-red-700"
+        role="menuitem"
+        tabIndex="-1"
+      >
+        Test
       </a>
     </div>
   );
--- src/Components/Header/DesktopMenu.js
+++ src/Components/Header/DesktopMenu.js
@@ -1,9 +1,16 @@
 // DesktopMenu.js
-import React from "react";
+import React, { useState } from "react";
 import NavLink from "./Navlink";
 import BookDropDown from "./BookDropDown";
+import AddBookForm from "./AddBookForm";
 
-const DesktopMenu = ({ toggleBookMenu, IsBookMenuOpen }) => {
+const DesktopMenu = ({ toggleBookMenu, isBookMenuOpen }) => {
+  const [isAddBookFormOpen, setAddBookFormOpen] = useState(false);
+
+  const toggleAddBookForm = () => {
+    setAddBookFormOpen((isAddBookFormOpen) => !isAddBookFormOpen);
+  };
+
   return (
     <div className="flex space-x-4">
       <NavLink href="#" label="Dashboard" />
@@ -11,13 +18,15 @@ const DesktopMenu = ({ toggleBookMenu, IsBookMenuOpen }) => {
         onClick={toggleBookMenu}
         className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
         aria-haspopup="true"
-        aria-expanded={IsBookMenuOpen}
+        aria-expanded={isBookMenuOpen}
       >
         Book Operations
       </button>
-      {IsBookMenuOpen && <BookDropDown />}
+      {isBookMenuOpen && <BookDropDown onAddBookClick={toggleAddBookForm} />}
       <NavLink href="#" label="Projects" />
       <NavLink href="#" label="Calendar" />
+
+      {isAddBookFormOpen && <AddBookForm />}
     </div>
   );
 };
